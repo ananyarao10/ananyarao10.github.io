3f662771bc118fae4426bef226d71a774c6559cf
Ananya Rao <106833228+ananyarao10@users.noreply.github.com>
2023-06-24 15:14:39 -0500

    Update graphic_design_gallery.js

--- graphic_design_gallery.js
+++ graphic_design_gallery.js
@@ -3,7 +3,7 @@ var imageThumbs = document.getElementById("image-thumbs");
 
 for (var i = 1; i <= 10; i++) {
     var thumb = document.createElement("img");
-    thumb.src = "graphic-design-photos/image" + i + ".jpg";
+    thumb.src = "image" + i + ".jpg";
     thumb.alt = "Image " + i;
     thumb.classList.add("thumb");
     imageThumbs.appendChild(thumb);
